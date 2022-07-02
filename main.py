eca9e7d2351e6df6b528c31a19c596820dde5c9a
Michele Tessari <michele.tessari12@gmail.com>
2022-07-02 12:42:17 +0200

    args fix

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from scenes import FunctionRender3D, FunctionRender2D, Scene
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="tool to visualize and explore 2D functions in realtime")
-    parser.add_argument("--mode", "-d", dest="mode2D", default=False, action="store_true", help="render the function on a 2D plane")
+    parser.add_argument("--2Dmode", "-2d", dest="mode2D", default=False, action="store_true", help="render the function on a 2D plane")
     args = parser.parse_args()
-    
+
     sys.argv = [sys.argv[0]] # modernGL takes some arguments, this line is needed to clean the argv
 
     scenes = [FunctionRender3D, FunctionRender2D]
